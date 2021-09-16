8b4dd8798d9d5f61c895ebb68f45c4d446eb1049
SilvijaSvegzde <83742823+SilvijaSvegzde@users.noreply.github.com>
2021-09-16 14:56:36 +1000

    updated forecast and current time

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
 import "./App.css";
 import React from "react";
-import Search from "./Search";
+import Weather from "./Weather";
 
 export default function App() {
+  let currentDate = new Date();
+  let hours = currentDate.getHours();
+  let minutes = currentDate.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let currentDay = days[currentDate.getDay()];
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
   return (
     <div className="App">
-      <h1>Weather Search Engine</h1>
-      <br />
-
-      <Search />
-
-      <h5> </h5>
+      <div className="weather-app-wrapper">
+        <div className="weather-app">
+          <h6>
+            Last updated: {currentDay}, {hours}:{minutes}
+          </h6>
+          <br />
+          <Weather />
+        </div>
+        <small>
+          <a
+            href="https://github.com/SilvijaSvegzde/weather-app-by-Silvija"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>
+          by Silvija Svegzde
+        </small>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+export default function Weather(props) {
+  let [display, setDisplay] = useState(false);
+  let [input, setInput] = useState(null);
+  let [temperature, setTemperature] = useState(null);
+  let [highest, setHighest] = useState(null);
+  let [lowest, setLowest] = useState(null);
+  let [description, setDescription] = useState(null);
+  let [humidity, setHumidity] = useState(null);
+  let [wind, setWind] = useState(null);
+  let [city, setCity] = useState(null);
+  let [country, setCountry] = useState(null);
+  let [icon, setIcon] = useState(null);
+
+  function showWeather(response) {
+    setDisplay(true);
+    setCity(response.data.name);
+    setCountry(response.data.sys.country);
+    setTemperature(Math.round(response.data.main.temp));
+    setHighest(Math.round(response.data.main.temp_max));
+    setLowest(Math.round(response.data.main.temp_min));
+    setDescription(response.data.weather[0].description);
+    setHumidity(response.data.main.humidity);
+    setWind(response.data.wind.speed);
+    setIcon(
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let units = "metric";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=46fd4cb2825699c13e293644a9027f76&units=${units}`;
+    axios.get(url).then(showWeather);
+  }
+
+  function cityInput(event) {
+    setInput(event.target.value);
+  }
+  if (display) {
+    return (
+      <div>
+        <form onSubmit={handleSubmit} id="city-form">
+          <div className="input-container">
+            <input
+              type="Search"
+              placeholder="Type a city name"
+              onChange={cityInput}
+              className="input-field"
+              id="city-input"
+              autoFocus="on"
+              autoComplete="off"
+            />
+          </div>
+        </form>
+
+        <h1>
+          {city}, {country}
+        </h1>
+        <h6> {description} </h6>
+
+        <div className="weather-temperature">
+          <img
+            src={icon}
+            alt={description}
+            className="weather-icon"
+            id="icon"
+          />
+          <span className="temperature" id="temperature-value">
+            {temperature}
+          </span>
+          <span className="units"> °C </span>
+        </div>
+        <h6 className="description">
+          H:<span id="highest">{highest}</span>°C | L:
+          <span id="lowest">{lowest}</span>°C
+        </h6>
+        <h6 className="description">Humidity: {humidity}% </h6>
+        <h6 className="description">Wind: {wind}km/h </h6>
+      </div>
+    );
+  } else {
+    return (
+      <form onSubmit={handleSubmit} id="city-form">
+        <div className="input-container">
+          <input
+            type="Search"
+            placeholder="Type a city name"
+            onChange={cityInput}
+            className="input-field"
+            id="city-input"
+            autoFocus="on"
+            autoComplete="off"
+          />
+        </div>
+      </form>
+    );
+  }
+}
